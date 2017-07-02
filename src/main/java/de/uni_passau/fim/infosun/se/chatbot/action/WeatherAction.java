5ece75969cb808fbfe56fcd4d787f8bf62528c06
Alice <alice@example.com>
2017-07-02 22:30:23 +0200

    Add WeatherAction
    The functionality has yet to be implemented.

---
+++ src/main/java/de/uni_passau/fim/infosun/se/chatbot/action/WeatherAction.java
@@ -0,0 +1,16 @@
+package de.uni_passau.fim.infosun.se.chatbot.action;
+
+import java.io.PrintStream;
+
+public class WeatherAction implements Action {
+    @Override
+    public boolean accept(String request) {
+        return request.contains("wetter");
+    }
+
+    @Override
+    public boolean run(String request, PrintStream out) {
+        out.println("Der Wetterfrosch ist heute leider krank :(");
+        return true;
+    }
+}
